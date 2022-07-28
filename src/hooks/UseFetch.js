89c692d9bf5827b391206d7dc1387ac3103fa3c3
Denis <payar3282@gmail.com>
2022-07-28 19:16:56 +0300

    style fix + usefetch try-catch

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react"
 export const useFetch = (url) => {
 	const [data, setData] = useState([])
 	useEffect(async () => {
-		const response = await fetch(url)
-		const result = await response.json()
-		setData(result)
+		try {
+			const response = await fetch(url)
+			const result = await response.json()
+			setData(result)
+		} catch (error) {
+			console.error(error)
+		}
 	}, [url])
 	window.scrollTo(0, 0)
 	return data
